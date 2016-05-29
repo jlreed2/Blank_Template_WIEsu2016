0249d86241c4ca1e74c9afff16e65c03bc43653f
Jennifer Reed <jlreed2@ufl.edu>
2016-05-28 21:43:14 -0700

    Add container div to body

--- index.php
+++ index.php
@@ -7,20 +7,21 @@
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
-    <header>
-        <h1>Blank WordPress Template</h1>
-        <p>This is my WordPress template.</p>
-    </header>
-    
-    <section>
-        <h2>Section Content-cool</h2>
-        <p>This is some cool section conent</p>
-    </section>
-    
-    <footer>
-        <h2>This is the footer</h2>
-        <p>This is come cool footer content</p>
-    </footer>
-    
+    <div class="container">
+        <header>
+            <h1>Blank WordPress Template</h1>
+            <p>This is my WordPress template.</p>
+        </header>
+
+        <section>
+            <h2>Section Content-cool</h2>
+            <p>This is some cool section conent</p>
+        </section>
+
+        <footer>
+            <h2>This is the footer</h2>
+            <p>This is come cool footer content</p>
+        </footer>
+    </div>
  </body>
 </html>
