f140b82c1b0d6dd535bc01ed31d63eb214eda949
Jennifer Reed <jlreed2@ufl.edu>
2016-05-28 21:27:11 -0700

    link stylesheet_url

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
     <meta charset="UTF-8">
     <title>Jennifer Reed Blank Template</title>
-    </head>
-    <body>
+    <!-- Links to our Style.css file -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+</head>
+<body>
         <h1>Blank WordPress Template</h1>
         <p>This is my WordPress template.</p>
-    </body>
+ </body>
 </html>
