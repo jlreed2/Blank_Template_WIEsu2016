8bd355fce1a6027ef1bd5bffa08036af69fdc03d
Jennifer Reed <jlreed2@ufl.edu>
2016-06-02 22:02:06 -0700

    Add thumbnail and image functionality

--- single.php
+++ single.php
@@ -7,7 +7,11 @@
                 while(have_posts()) {
                    /*OUR DATA CONTEXT IS DEFINED */
                     the_post(); ?>
-            
+                    if ( has_post_thumbnail() ) { ?>
+                        <div class="post-thumbnail">
+                            <?php the_post_thumbnail('large'); ?>
+                        </div>
+                    <?php } ?>
                     <h2><?php the_title(); ?></h2>
                     <?php the_content();
                 }
